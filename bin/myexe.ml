f1e198173ce726d7d2074443172fcc7185ae6d8b
Naomi Spargo <nspargo@andrew.cmu.edu>
2022-08-01 16:10:58 +0100

    run working

--- bin/myexe.ml
+++ bin/myexe.ml
@@ -11,8 +11,8 @@ let signature ~runtime ~module_name namespace (s : Gospel.Tast.signature) =
 
                                                           *)
   Report.emit_warnings Fmt.stderr translated;
-  print_endline("translations after phase 1 are:");
-  Core.Sexp.output_hum stdout (Drv.sexp_of_sil translated.translations); 
+  (* print_endline("translations after phase 1 are:");
+     Core.Sexp.output_hum stdout (Drv.sexp_of_sil translated.translations); *)
   let stm =  Phase2.stm translated in
   Phase3.structure runtime stm 
 (*      (map_of_list [("field1", Ast3.List Ast3.Integer);
@@ -23,17 +23,16 @@ let signature ~runtime ~module_name namespace (s : Gospel.Tast.signature) =
 
 let generate path output =
   let module_name = Ortac_core.Utils.module_name_of_path path in
-  let _output = Format.formatter_of_out_channel output in
+  let output = Format.formatter_of_out_channel output in
   Gospel.Parser_frontend.parse_ocaml_gospel path
   |> Ortac_core.Utils.type_check [] path
   |> fun (env, sigs) ->
   assert (List.length env = 1);
- print_endline("typed signature items are");
-    Core.Sexp.output_hum stdout (Gospel.Tast.sexp_of_signature sigs); 
+ (* print_endline("typed signature items are");
+    Core.Sexp.output_hum stdout (Gospel.Tast.sexp_of_signature sigs); *)
     signature ~runtime:"Ortac_runtime" ~module_name (List.hd env)
       sigs
-     |> (fun _ -> print_endline "done")
-     (* Fmt.pf output "%a@." Ppxlib_ast.Pprintast.structure *)
+     |> Fmt.pf output "%a@." Ppxlib_ast.Pprintast.structure 
 
 
 
@@ -41,7 +40,7 @@ let generate path output =
 that this doesn't work, ask jan*)
 
 let main () =
-   let inpath = "example.mli" in
+   let inpath = "atom.mli" in
 (*let outpath = stdout in*)
  (* let channel = open_out outpath in*)
 let channel = stdout in generate inpath channel
